6c21dc3d7ab0949cc7ed3dcbe3804e10a449d7de
Vojtech Jurok <xjurokv00@stud.fit.vut.cz>
2026-03-14 14:20:48 +0100

    Add basic arithmetic function declarations

---
+++ math_lib.h
@@ -0,0 +1,38 @@
+#ifndef MATH_LIB_B
+#define MATH_LIB_B
+
+
+/** 
+ * @brief Adds two numbers together.
+ * @param a First number to be added.
+ * @param b Second nuber to be added.
+ * @return The sum of the two numbers.
+*/
+double add(double a, double b);
+
+
+/**
+ * @brief Substracts two numbers.
+ * @param a Number to be substracted from.
+ * @param b Number to substract.
+ * @return The difference between the two numbers.
+ */
+double substract(double a, double b);
+
+/**
+ * @brief Multiplies two numbers.
+ * @param a First number to multiply. 
+ * @param b Second number to multiply.
+ * @return The result of multipling a and b.
+ */
+double multiply(double a, double b);
+
+/**
+ * @brief Divides two numbers.
+ * @param a Number to be divided from.
+ * @param b Number to divide.
+ * @return The result of dividing a and b.
+ */
+double divide(double a, double b);
+
+#endif
