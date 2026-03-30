b92e0dbba37ff208e4c2f7639d09c9723e32ead0
Petr Hanzlik <xhanzlp00@stud.fit.vut.cz>
2026-03-30 12:02:27 +0200

    add function to number buttons
    add function on clicked for button ".", "0", "00", "1", "2", "3", "4",
    "5", "6", "7", "8", "9"

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -7,3 +7,70 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::Calcul
 }
 
 MainWindow::~MainWindow() { delete ui; }
+
+void MainWindow::handleTextChange(QString change)
+{
+    lineEditText += change;
+    ui->result->setText(lineEditText);
+}
+
+void MainWindow::on_button0_clicked()
+{
+    handleTextChange("0");
+}
+
+void MainWindow::on_button00_clicked()
+{
+    handleTextChange("00");
+}
+
+void MainWindow::on_buttonFloatDot_clicked()
+{
+    handleTextChange(".");
+}
+
+void MainWindow::on_button3_clicked()
+{
+    handleTextChange("3");
+}
+
+void MainWindow::on_button2_clicked()
+{
+    handleTextChange("2");
+}
+
+void MainWindow::on_button1_clicked()
+{
+    handleTextChange("1");
+}
+
+void MainWindow::on_button6_clicked()
+{
+    handleTextChange("6");
+}
+
+void MainWindow::on_button5_clicked()
+{
+    handleTextChange("5");
+}
+
+void MainWindow::on_button4_clicked()
+{
+    handleTextChange("4");
+}
+
+void MainWindow::on_button9_clicked()
+{
+    handleTextChange("9");
+}
+
+void MainWindow::on_button8_clicked()
+{
+    handleTextChange("8");
+}
+
+void MainWindow::on_button7_clicked()
+{
+    handleTextChange("7");
+}
+
--- src/gui/mainwindow.h
+++ src/gui/mainwindow.h
@@ -16,7 +16,75 @@ class MainWindow : public QMainWindow {
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+   private slots:
+    /**
+     * @brief Add "0" to our lineEdit (result)
+     */
+    void on_button0_clicked();
+
+    /**
+     * @brief Add "00" to our lineEdit (result)
+     */
+    void on_button00_clicked();
+
+    /**
+     * @brief Add "." tou our lineEdit (result)
+     */
+    void on_buttonFloatDot_clicked();
+
+    /**
+     * @brief Add "3" tou our lineEdit (result)
+     */
+    void on_button3_clicked();
+
+    /**
+     * @brief Add "2" tou our lineEdit (result)
+     */
+    void on_button2_clicked();
+
+    /**
+     * @brief Add "1" tou our lineEdit (result)
+     */
+    void on_button1_clicked();
+
+    /**
+     * @brief Add "6" tou our lineEdit (result)
+     */
+    void on_button6_clicked();
+
+    /**
+     * @brief Add "5" tou our lineEdit (result)
+     */
+    void on_button5_clicked();
+
+    /**
+     * @brief Add "4" tou our lineEdit (result)
+     */
+    void on_button4_clicked();
+
+    /**
+     * @brief Add "9" tou our lineEdit (result)
+     */
+    void on_button9_clicked();
+
+    /**
+     * @brief Add "8" tou our lineEdit (result)
+     */
+    void on_button8_clicked();
+
+    /**
+     * @brief Add "7" tou our lineEdit (result)
+     */
+    void on_button7_clicked();
+
    private:
+    /**
+     * @brief Take string and add to existing string, after that set that string to lineEdit (result)
+     * @param change - string that you want add
+     */
+    void handleTextChange (QString change);
+
     Ui::Calculator *ui;
+    QString lineEditText = "";
 };
 #endif  // MAINWINDOW_H
